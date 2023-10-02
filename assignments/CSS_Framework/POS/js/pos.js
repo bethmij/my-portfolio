c208d86839d456f26faa2e83596a1202c804879e
Dinarbrooke <bethmij@gmail.com>
2023-10-02 22:21:33 +0530

    modified portfolio

--- assignments/CSS_Framework/POS/js/pos.js
+++ assignments/CSS_Framework/POS/js/pos.js
@@ -6,6 +6,7 @@
      element.addEventListener("click", function (){
          document.querySelector("#customer").style.display = "none";
          document.querySelector("#order").style.display = "none";
+         document.querySelector("#home").style.display = "none";
          var item = document.querySelector("#item");
          item.style.display = "block";
          item.className = element.className.replace("d-none", "d-block");
@@ -21,6 +22,7 @@
      element2.addEventListener("click", function (){
          document.querySelector("#customer").style.display = "none";
          document.querySelector("#item").style.display = "none";
+         document.querySelector("#home").style.display = "none";
          var order = document.querySelector("#order");
          order.style.display = "block";
          order.className = element2.className.replace("d-none", "d-block");
@@ -31,12 +33,28 @@
  });
 
  var customBtn = document.querySelectorAll(".btnCustomer");
- customBtn.forEach(function(element) {
+ customBtn.forEach(function(element3) {
 
-     element.addEventListener("click", function (){
+     element3.addEventListener("click", function (){
          document.querySelector("#item").style.display = "none";
          document.querySelector("#order").style.display = "none";
-         document.querySelector("#customer").style.display = "block";
+         document.querySelector("#home").style.display = "none";
+         var customer = document.querySelector("#customer");
+         customer.style.display = "block";
+         customer.className = element3.className.replace("d-none", "d-block");
+         customer.className = customer.className.replace("ms-2", "ms-0");
+         customer.className = customer.className.replace("mb-5", "mb-0");
+     });
+ });
+
+ var homeBtn = document.querySelectorAll(".btnHome");
+ homeBtn.forEach(function(element4) {
+
+     element4.addEventListener("click", function (){
+         document.querySelector("#item").style.display = "none";
+         document.querySelector("#order").style.display = "none";
+         document.querySelector("#customer").style.display = "none";
+         document.querySelector("#home").style.display = "block";
      });
  });
 
